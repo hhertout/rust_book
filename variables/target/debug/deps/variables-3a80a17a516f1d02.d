b688a8a537e1af5b0187708aa290471848facb66
Hugues Hertout <109511726+hhertout@users.noreply.github.com>
2023-04-03 13:57:37 +0200

    Delete variables directory

--- variables/target/debug/deps/variables-3a80a17a516f1d02.d
+++
@@ -1,5 +0,0 @@
-/Users/hugueshertout/Rust/rust_book/variables/target/debug/deps/variables-3a80a17a516f1d02: src/main.rs
-
-/Users/hugueshertout/Rust/rust_book/variables/target/debug/deps/variables-3a80a17a516f1d02.d: src/main.rs
-
-src/main.rs:
